91d2d74b3190f99da435ac6b55e3a8668d11bb6a
PrasanthKumar0 <142900866+PrasanthKumar0@users.noreply.github.com>
2024-01-02 08:57:03 +0530

    Update reuse_jenkins.sh

--- reuse_jenkins.sh
+++ reuse_jenkins.sh
@@ -28,44 +28,34 @@ generate_ssh_keys() {
     local user=$1
     local host=$2
 
-    run_remote_command "$user" "$host" "sudo su - jenkins -c 'ssh-keygen -t rsa'"
+    run_remote_command "$user" "$host" "ssh-keygen -t rsa"
 }
 
-# Copy SSH Public Key from Master to Slave
+# Copy SSH Public Key from one machine to another
 copy_ssh_public_key() {
-    local master_user=$1
-    local master_host=$2
-    local slave_user=$3
-    local slave_host=$4
+    local source_user=$1
+    local source_host=$2
+    local dest_user=$3
+    local dest_host=$4
 
-    MASTER_SSH_PUBLIC_KEY=$(run_remote_command "$master_user" "$master_host" "sudo cat /var/lib/jenkins/.ssh/id_rsa.pub")
-    run_remote_command "$slave_user" "$slave_host" "echo \"$MASTER_SSH_PUBLIC_KEY\" | sudo tee -a /var/lib/jenkins/.ssh/authorized_keys"
-}
-
-# Verify SSH Connection from Master to Slave
-verify_ssh_connection() {
-    local master_user=$1
-    local master_host=$2
-    local slave_user=$3
-    local slave_host=$4
-
-    run_remote_command "$master_user" "$master_host" "sudo su - jenkins -c 'ssh $slave_user@$slave_host'"
+    SOURCE_SSH_PUBLIC_KEY=$(run_remote_command "$source_user" "$source_host" "cat ~/.ssh/id_rsa.pub")
+    run_remote_command "$dest_user" "$dest_host" "echo \"$SOURCE_SSH_PUBLIC_KEY\" >> ~/.ssh/authorized_keys"
 }
 
 # Install Jenkins on Master
-install_jenkins <master_username> <master_ip>
+install_jenkins master_user 192.168.1.100
 
 # Generate SSH Keys for Jenkins Master
-generate_ssh_keys <master_username> <master_ip>
-
-# Install Jenkins on Slave
-install_jenkins <slave_username> <slave_ip>
+generate_ssh_keys master_user 192.168.1.100
 
 # Copy Jenkins Master SSH Public Key to Slave
-copy_ssh_public_key <master_username> <master_ip> <slave_username> <slave_ip>
+copy_ssh_public_key master_user 192.168.1.100 slave_user 192.168.1.101
+
+# Install Jenkins on Slave
+install_jenkins slave_user 192.168.1.101
 
 # Verify SSH Connection from Master to Slave
-verify_ssh_connection <master_username> <master_ip> <slave_username> <slave_ip>
+run_remote_command master_user 192.168.1.100 "ssh slave_user@192.168.1.101 exit"
 
 # Exit Jenkins User Shell on Master
-run_remote_command <master_username> <master_ip> "exit"
+run_remote_command master_user 192.168.1.100 "exit"
