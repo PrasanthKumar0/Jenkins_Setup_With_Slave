93e1ae12bd60980e045e15720734dc2bf2d92db4
PrasanthKumar0 <142900866+PrasanthKumar0@users.noreply.github.com>
2024-01-02 08:14:58 +0530

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "redhat" {
+  most_recent = true
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
